e0379017cae4f6e6c593501af27ff4f21c8592e5
Pragya <sabharwalpragya@gmail.com>
2022-01-20 00:59:52 +0530

    chore: link change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Card } from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <a href=" https://spacetagram1.netlify.app/">View on Github</a>
+      <a href="https://github.com/pragyasabharwal/spacetagram">View on Github</a>
       <Card></Card>
     </div>
   );

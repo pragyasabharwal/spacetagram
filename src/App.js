f05e37a5a3d9a1b23cd76bfbc9724c5bc5cbb219
Pragya <sabharwalpragya@gmail.com>
2022-01-20 00:59:24 +0530

    chore: link for src code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Card } from "./components/Card";
 function App() {
   return (
     <div className="App">
+      <a href=" https://spacetagram1.netlify.app/">View on Github</a>
       <Card></Card>
     </div>
   );
